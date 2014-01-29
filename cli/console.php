1b3f4a02c681f7ae33d692f3cb4671b314b4bd95
Paul M. Jones <pmjones88@gmail.com>
2014-01-28 21:33:55 -0600

    stop using kernel scripts, embed boot logic in project

--- cli/console.php
+++ cli/console.php
@@ -8,7 +8,22 @@
  * @license http://opensource.org/licenses/bsd-license.php BSD
  * 
  */
+use Aura\Project_Kernel\ProjectContainer;
 
-// hand off to the cli kernel script and get back the exit status
-$status = require dirname(__DIR__) . '/vendor/aura/cli-kernel/scripts/kernel.php';
+// the project base directory
+$base = dirname(__DIR__);
+
+// set up autoloader
+$loader = require "$base/vendor/autoload.php";
+$loader->add('', "{$base}/src");
+
+// load environment modifications
+require "{$base}/config/_env.php";
+
+// create the project container
+$di = ProjectContainer::factory($base, $loader, $_ENV, null);
+
+// create and invoke a cli kernel
+$cli_kernel = $di->newInstance('Aura\Cli_Kernel\CliKernel');
+$status = $cli_kernel();
 exit($status);
