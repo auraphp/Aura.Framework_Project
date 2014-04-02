34ad1123de571a1bfe40306cc45885d7fc710734
Paul M. Jones <pmjones88@gmail.com>
2014-04-02 13:41:53 -0500

    use newKernel()

--- cli/console.php
+++ cli/console.php
@@ -8,22 +8,11 @@
  * @license http://opensource.org/licenses/bsd-license.php BSD
  * 
  */
-use Aura\Project_Kernel\ProjectContainer;
-
-// the project base directory
-$base = dirname(__DIR__);
-
-// set up autoloader
-$loader = require "$base/vendor/autoload.php";
-$loader->add('', "{$base}/src");
-
-// load environment modifications
-require "{$base}/config/_env.php";
-
-// create the project container
-$di = ProjectContainer::factory($base, $loader, $_ENV, null);
-
-// create and invoke a cli kernel
-$cli_kernel = $di->newInstance('Aura\Cli_Kernel\CliKernel');
-$status = $cli_kernel();
+$path = dirname(__DIR__);
+require "{$path}/vendor/autoload.php";
+$kernel = (new \Aura\Project_Kernel\Factory)->newKernel(
+    $path,
+    'Aura\Cli_Kernel\CliKernel'
+);
+$status = $kernel();
 exit($status);
--- web/index.php
+++ web/index.php
@@ -8,21 +8,11 @@
  * @license http://opensource.org/licenses/bsd-license.php BSD
  * 
  */
-use Aura\Project_Kernel\ProjectContainer;
 
-// the project base directory
-$base = dirname(__DIR__);
-
-// set up autoloader
-$loader = require "$base/vendor/autoload.php";
-$loader->add('', "{$base}/src");
-
-// load environment modifications
-require "{$base}/config/_env.php";
-
-// create the project container
-$di = ProjectContainer::factory($base, $loader, $_ENV, null);
-
-// create and invoke a web kernel
-$web_kernel = $di->newInstance('Aura\Web_Kernel\WebKernel');
-$web_kernel();
+$path = dirname(__DIR__);
+require "{$path}/vendor/autoload.php";
+$kernel = (new \Aura\Project_Kernel\Factory)->newKernel(
+    $path,
+    'Aura\Web_Kernel\WebKernel'
+);
+$kernel();
