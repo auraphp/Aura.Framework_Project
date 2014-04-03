094301c31a659cb3815340fb5d2c053d93f6f62a
Paul M. Jones <pmjones88@gmail.com>
2014-04-02 23:37:04 -0500

    improve local testing

--- tests/project/FrameworkProjectTest.php
+++ tests/project/FrameworkProjectTest.php
@@ -1,13 +1,21 @@
 <?php
 namespace Aura\Framework_Project;
 
-class CliProjectTest extends \PHPUnit_Framework_TestCase
+class FrameworkProjectTest extends \PHPUnit_Framework_TestCase
 {
     public function testCli()
     {
         $console = dirname(dirname(__DIR__)) . '/cli/console.php';
         $actual = shell_exec("php {$console} hello");
         $expect = 'Hello World!' . PHP_EOL;
+        $this->assertSame($expect, $actual);
+    }
+
+    public function testWeb()
+    {
+        $url = "http://localhost:8080/index.php";
+        $actual = file_get_contents($url);
+        $expect = 'Hello World!';
         $this->assertSame($expect, $actual);
     }
 }
